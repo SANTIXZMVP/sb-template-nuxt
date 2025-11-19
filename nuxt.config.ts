9c93c97bddcaa88c826f3db397792d3ef343052c
Stefano Biddau <44703164+stefanBid@users.noreply.github.com>
2025-11-19 22:42:01 +0100

    feat: add ESLint configuration for code formatting and linting (#2)
    * feat: add ESLint configuration for code formatting and linting
    
    * feat: update ESLint configuration with ignored paths and rules
    
    * feat: update app.vue formatting and enhance package.json with linting scripts
    
    * fix: remove unnecessary empty line in ESLint configuration
    
    * fix: adjust formatting in app.vue and reorder properties in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image']
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
