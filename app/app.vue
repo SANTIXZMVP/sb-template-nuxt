0be5f04a68f85bcd64a78c47afc3f09573e844a7
Stefano Biddau <44703164+stefanBid@users.noreply.github.com>
2025-11-19 23:48:42 +0100

    Feat/add project structture (#5)
    * feat: add project structure with layout and main template
    
    * feat: implement main layout and error handling pages
    
    * fix: adjust main container width in error page
    
    * fix: update class name for transition effect in default layout

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <h1 class=" ty-app-impact text-app-contrast h-[200vh]">
-      Welcome to My Nuxt App
-    </h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
